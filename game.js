4c7530624f68a78f5c90645f3c95877ed0911f25
Shawn Meas <shawn@smeas.info>
2022-08-09 09:14:06 -0400

    Implemented playRound function

--- game.js
+++ game.js
@@ -3,16 +3,43 @@ function getComputerChoice() {
     let computerChoice;
     if(rand == 0)
     {
-        computerChoice = "rock";
+        computerChoice = 'rock';
     }
     else if(rand == 1)
     {
-        computerChoice = "paper";
+        computerChoice = 'paper';
     }
     else if(rand == 2)
     {
-        computerChoice = "scissors";
+        computerChoice = 'scissors';
     }
 
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection == computerSelection)
+    {
+        result = 'draw';
+        return result;
+    }
+    else if(playerSelection == 'rock')
+    {
+        result = computerSelection == 'scissors' ? 'win' : computerSelection == 'paper' ? 'loss' : 'error';
+    }
+    else if(playerSelection == 'paper')
+    {
+        result = computerSelection == 'rock' ? 'win' : computerSelection == 'scissors' ? 'loss' : 'error';
+    }
+    else if(playerSelection == 'scissors')
+    {
+        result = computerSelection == 'paper' ? 'win' : computerSelection == 'rock' ? 'loss' : 'error';
+    }
+    return result;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
